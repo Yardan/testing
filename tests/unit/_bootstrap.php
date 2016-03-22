4325d19f86be1e40071969497e4e0e2505b90701
Daniar <daniar.otorbaev@gmail.com>
2016-03-22 15:38:22 +0200

    fix

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 // Here you can initialize variables that will be available to your tests
-$config = \yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../config/web.php'),
-    require(__DIR__ . '/config/config.php')
-);
-$application = new yii\web\Application(requ);
+$application = new yii\web\Application(require(__DIR__.'/../config/unit.php'));
