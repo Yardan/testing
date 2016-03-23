4e85f468a5b8dc6ab915379b8bf1a71af7a2e3ac
Daniar <daniar.otorbaev@gmail.com>
2016-03-23 18:38:18 +0200

    add newTest #4 add marks

---
+++ tests/unit/FutureTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tests\unit;
+
+use Codeception\TestCase\Test;
+
+class FutureTest extends Test
+{
+    /**
+     * @var \UnitTeste
+     */
+    protected $tester;
+
+    public function testSomeFuture()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testOtherFuture()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSaveToMSSQL()
+    {
+        if(!extension_loaded('mssql')){
+            $this->markTestSkipped('The MSSQL extension is not available!');
+        }
+    }
+}
